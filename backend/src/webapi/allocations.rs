56e0b43d03f25fe7620c474324c5d941a1a0edfa
Moritz Kempe <moritzkempe@mailbox.org>
2025-11-19 18:14:24 +0100

    added sqlx_conditional_queries to allocations

--- backend/src/webapi/allocations.rs
+++ backend/src/webapi/allocations.rs
@@ -5,6 +5,7 @@ use rocket::response::status::BadRequest;
 use rocket::serde::json::Json;
 use rocket::{delete, get, patch, post, State};
 use sqlx::query_as;
+use sqlx_conditional_queries::conditional_query_as;
 
 #[get("/allocations?<limit>&<page>")]
 pub(crate) async fn get_allocation(app_state: &State<api::AppStatePointer>,
@@ -14,12 +15,23 @@ pub(crate) async fn get_allocation(app_state: &State<api::AppStatePointer>,
         let app_state = app_state.lock().await;
         app_state.get_storage_system().clone()
     };
-    let limit = limit.unwrap_or(12);
-    let page = page.unwrap_or(0);
-    // row_id in sqlite starts with 1, therefore the following pagination code is correct
-    let start = limit * page + 1;
-    let end = limit * (page + 1);
-    match query_as!(Allocation, "SELECT * FROM allocations WHERE id BETWEEN ?1 AND ?2;", start, end).fetch_all(storage_system.get_database()).await {
+
+    // calculate pagination
+    let new_page = page.unwrap_or(0);
+    let new_limit = limit.unwrap_or(64);
+    let start = new_limit * new_page + 1;
+    let end = new_limit * (new_page + 1);
+
+    match conditional_query_as!(Allocation,
+        r#"SELECT *
+        FROM allocations
+        {#pagination};"#,
+        #pagination = match limit {
+            Some(_) =>
+                "WHERE id BETWEEN {start} AND {end}",
+            None => "",
+        },
+    ).fetch_all(storage_system.get_database()).await {
         Ok(result) => {
             Ok(Json(result))
         }
