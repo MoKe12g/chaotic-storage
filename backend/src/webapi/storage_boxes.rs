6a0422882b565397377ea02f7ea384aae90c0192
Moritz Kempe <moritzkempe@mailbox.org>
2025-12-02 00:40:56 +0100

    added filter to storage_boxes

--- backend/src/webapi/storage_boxes.rs
+++ backend/src/webapi/storage_boxes.rs
@@ -7,10 +7,13 @@ use rocket::{delete, get, patch, post, State};
 use sqlx::query_as;
 use sqlx_conditional_queries::conditional_query_as;
 
-#[get("/storage_boxes?<limit>&<page>")]
+#[get("/storage_boxes?<limit>&<page>&<id>&<place>&<item_type>")]
 pub(crate) async fn get_storage_box(app_state: &State<api::AppStatePointer>,
                                     limit: Option<i64>,
-                                    page: Option<i64>) -> Result<Json<Vec<StorageBox>>, BadRequest<Json<MessageResponse>>> {
+                                    page: Option<i64>,
+                                    id: Option<i64>,
+                                    place: Option<String>,
+                                    item_type: Option<i64>) -> Result<Json<Vec<StorageBox>>, BadRequest<Json<MessageResponse>>> {
     let storage_system = {
         let app_state = app_state.lock().await;
         app_state.get_storage_system().clone()
@@ -25,11 +28,30 @@ pub(crate) async fn get_storage_box(app_state: &State<api::AppStatePointer>,
     match conditional_query_as!(StorageBox,
         r#"SELECT *
         FROM storage_boxes
+        WHERE 1
+        {#id}
+        {#place}
+        {#item_type}
         {#pagination}
         ORDER BY ID ASC;"#,
+        #id = match id.as_ref() {
+            Some(_) =>
+                "AND id = {id}",
+            None => "",
+        },
+        #place = match place.as_ref() {
+            Some(_) =>
+                "AND place LIKE '%' || {place} || '%'",
+            None => "",
+        },
+        #item_type = match item_type.as_ref() {
+            Some(_) =>
+                "AND item_type = {item_type}",
+            None => "",
+        },
         #pagination = match limit {
             Some(_) =>
-                "WHERE id BETWEEN {start} AND {end}",
+                "AND id BETWEEN {start} AND {end}",
             None => "",
         },
     ).fetch_all(storage_system.get_database()).await {
