e331b281ade47ad880eca151dc60d8feb0e29c5c
Moritz Kempe <moritzkempe@mailbox.org>
2025-12-02 00:50:55 +0100

    Revert "added filter to allocation web api"
    This reverts commit bef6c8ddc29614114fa94761e96de3be8fab9c22.
    
    # Conflicts:
    #	backend/.sqlx/query-051ed51f73061cd3eee0176fcc60ce46e7e7f492f7581491af53d8966d2497ef.json
    #	backend/.sqlx/query-0e1bd0f8f58532ec765641b1b3b773ab134908140748ff7829928cca5f5e537a.json
    #	backend/.sqlx/query-1b3ee3dd814ba7394c3702e4aca39bb3c798346ad3a76ef68b3818fdd1640e11.json
    #	backend/.sqlx/query-460c5354f5325e1a9a4177d4042d495bd62e032bde5d4aacb82a5190fc6f18d5.json
    #	backend/.sqlx/query-4bc81dfec2a7f48857910a6e0cccbe7df03acb26a10de06cc7e8493630549b72.json
    #	backend/.sqlx/query-63fa81d00f673806a537e6487f382b8e74036cbc3d1d0a78952826ed383b4cbf.json
    #	backend/.sqlx/query-79879975dcef56c6d6afc6d940e3ffdb3079b045b3178bc713109217b4b3e1c8.json
    #	backend/.sqlx/query-87c061d800dffae71e69665c9f46709329b16d30fb152af99c2eda3c9338d163.json
    #	backend/.sqlx/query-8e3e1ae8ebc0f93e36423a0b1e74fc1eb46f3641aed5a11e2364130ab664e2d2.json
    #	backend/.sqlx/query-98857398dd14fce13349fc3dce27c735e8d85f847de880c900ba4f79bd34acde.json
    #	backend/.sqlx/query-b076e881b85b1c905161d51537264a0e1ba4fc4f172a175526f07f553493adb1.json
    #	backend/.sqlx/query-c4c2bd30b823106b8987612e8f75575dce5f430329b2cd7a2664af7e92ccd1fd.json
    #	backend/.sqlx/query-cb7d017512830831e0d773999cc0527028af6474254e9428460d1bf2a6e67726.json
    #	backend/.sqlx/query-ddf64845e1faa74d4490e3cc6859c7d9fd8973ae402814a78d1549183f3228a5.json
    #	backend/.sqlx/query-e1ee5760f1e77a986de81a0b6251364b0e812e7e5d0fa4bbded4e9998dc09cb3.json
    #	backend/.sqlx/query-ec03a86a5e7ac7739cd9d15e5f50a34408ebaa408c0eb29a0198c0135e7ec173.json
    #	backend/.sqlx/query-f28eda7a144fb4458ae8998515c7f448d9e0a147744627c9813ce39a0105db34.json
    #	backend/.sqlx/query-f43cbb7344d4e8035a019ef05a754956723683cf7bc5aebd79790d7111c71c2f.json
    #	backend/.sqlx/query-fc4789f282d8447b7893e66f1da5d8d6eb0cd2f24d89ee5498073bc8d5f922d8.json
    #	backend/.sqlx/query-fdba2e9d9a6bc1e0c1931dac85cad874f8045d220caf4eba2a1d6a0f81462dd5.json
    #	backend/src/webapi/allocations.rs

--- backend/src/webapi/allocations.rs
+++ backend/src/webapi/allocations.rs
@@ -7,14 +7,10 @@ use rocket::{delete, get, patch, post, State};
 use sqlx::query_as;
 use sqlx_conditional_queries::conditional_query_as;
 
-#[get("/allocations?<limit>&<page>&<storage_box_id>&<can_be_outside>&<category_id>&<description>")]
+#[get("/allocations?<limit>&<page>")]
 pub(crate) async fn get_allocation(app_state: &State<api::AppState>,
                                    limit: Option<i64>,
-                                   page: Option<i64>,
-                                   storage_box_id: Option<i64>,
-                                   can_be_outside: Option<bool>,
-                                   category_id: Option<i64>,
-                                   description: Option<String>) -> Result<Json<Vec<Allocation>>, BadRequest<Json<MessageResponse>>> {
+                                   page: Option<i64>) -> Result<Json<Vec<Allocation>>, BadRequest<Json<MessageResponse>>> {
     let storage_system = {
         let app_state = app_state.lock().await;
         app_state.get_storage_system().clone()
@@ -29,36 +25,11 @@ pub(crate) async fn get_allocation(app_state: &State<api::AppState>,
     match conditional_query_as!(Allocation,
         r#"SELECT *
         FROM allocations
-        WHERE 1
-        {#storage_box_id}
-        {#can_be_outside}
-        {#category_id}
-        {#description}
         {#pagination}
         ORDER BY ID ASC;"#,
-        #storage_box_id = match storage_box_id {
-            Some(_) =>
-            "AND storage_box_id = {storage_box_id}",
-            None => "",
-        },
-        #can_be_outside = match can_be_outside {
-            Some(_) =>
-            "AND can_be_outside = {can_be_outside}",
-            None => "",
-        },
-        #category_id = match category_id {
-            Some(_) =>
-            "AND category_id = {category_id}",
-            None => "",
-        },
-        #description = match description.as_ref() {
-            Some(_) =>
-            "AND description LIKE '%' || {description} || '%'",
-            None => "",
-        },
         #pagination = match limit {
             Some(_) =>
-                "AND id BETWEEN {start} AND {end}",
+                "WHERE id BETWEEN {start} AND {end}",
             None => "",
         },
     ).fetch_all(storage_system.get_database()).await {
