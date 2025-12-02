9df207153b548ac4f1fa30a885633314aa1115d0
Moritz Kempe <moritzkempe@mailbox.org>
2025-12-02 01:04:12 +0100

    re-done pagination via offset and limit on item_type

--- backend/src/webapi/item_type.rs
+++ backend/src/webapi/item_type.rs
@@ -18,8 +18,7 @@ pub(crate) async fn get_item_type(app_state: &State<api::AppState>,
     // calculate pagination
     let new_page = page.unwrap_or(0);
     let new_limit = limit.unwrap_or(64);
-    let start = new_limit * new_page + 1;
-    let end = new_limit * (new_page + 1);
+    let offset = new_limit * new_page;
 
     match conditional_query_as!(ItemType,
         r#"SELECT *
@@ -27,8 +26,8 @@ pub(crate) async fn get_item_type(app_state: &State<api::AppState>,
         WHERE 1
         {#id}
         {#storage_property}
-        {#pagination}
-        ORDER BY ID ASC;"#,
+        ORDER BY ID ASC
+        {#pagination};"#,
         #id = match id.as_ref() {
             Some(_) =>
                 "AND id = {id}",
@@ -41,7 +40,7 @@ pub(crate) async fn get_item_type(app_state: &State<api::AppState>,
         },
         #pagination = match limit {
             Some(_) =>
-                "AND id BETWEEN {start} AND {end}",
+                "LIMIT {new_limit} OFFSET {offset}",
             None => "",
         },
     ).fetch_all(storage_system.get_database()).await {
